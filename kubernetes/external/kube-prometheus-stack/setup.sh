9b2996dbc52101f96dcb91256000535260a744a3
Pierre Peixoto <pro.sen.peixoto@gmail.com>
2025-04-27 19:31:58 +0200

    Pushing service.yaml to try to forward grafana

--- kubernetes/external/kube-prometheus-stack/setup.sh
+++ kubernetes/external/kube-prometheus-stack/setup.sh
@@ -1,7 +1,6 @@
 helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
 helm repo update
 helm upgrade --install kube-prometheus-stack prometheus-community/kube-prometheus-stack -f prometheus-values.yaml --create-namespace --namespace monitoring
-
-kubectl port-forward -n monitoring deployment/prometheus-grafana 3000
+kubectl apply -f volume.yaml --namespace monitoring
 
 kubectl --namespace monitoring get pods -l "release=kube-prometheus-stack"
